e147f70f0d5e9767cd22d2d7ebd96b1785e551a4
sidhrthmnn <72742539+sidhrthmnn@users.noreply.github.com>
2021-03-16 17:57:44 +0000

    Player and Enemy Added
    Added FPS
    Added Zombies
    Added a  scene to test the scanner

---
+++ Zatochi group work/Assets/Scrpits/bullet.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+
+
+    public float speed = 0.001f;
+    Vector3 lastPosition;
+    public float lifetime = 5.0f;
+    public Transform gun;
+    public GameObject blood;
+    public AI enemyData;
+    
+
+    // Start is called before the first frame update
+
+    void OnEnable()
+    {
+        lifetime = 2.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //lastPosition = this.transform.position;
+        transform.position += transform.forward * speed * Time.deltaTime;
+        //transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        lifetime -= Time.deltaTime;
+        if (lifetime <= 0)
+        {
+            this.gameObject.SetActive(false);
+            transform.position = gun.transform.position;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            this.gameObject.SetActive(false);
+            transform.position = gun.transform.position;
+            
+        }
+    }
+
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if(other.tag == "Enemy")
+    //    {
+    //        Debug.Log(enemyData.enemyHealth);
+    //        enemyData.enemyHealth -= 20;
+    //        Debug.Log(enemyData.enemyHealth);
+    //        Debug.Log("Hit");
+
+    //    }
+    //}
+}
+
+
+
+
---
+++ Zatochi group work/Assets/Scrpits/gun.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun : MonoBehaviour
+{
+
+    public GameObject bullets;
+    public GameObject pool;
+    public List<GameObject> list;
+    void Start()
+    {
+        
+        for(int i = 0; i < 10; i++)
+        {
+            pool = Instantiate(bullets);
+            pool.GetComponent<bullet>().gun = this.transform;
+            list.Add(pool);
+            pool.SetActive(false);
+        }
+    }
+
+   
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject currentbullet = shoot();
+            if(currentbullet !=null)
+            {
+                currentbullet.transform.position = this.gameObject.transform.position;
+                currentbullet.transform.rotation = this.gameObject.transform.rotation;
+            }
+            
+        }
+        
+    }
+    
+    public GameObject shoot()
+    {
+        foreach (GameObject bullet in list)
+        {
+            if (!bullet.activeSelf)
+            {
+                bullet.SetActive(true);
+                return bullet;
+                
+            }
+            
+        }
+        return null;
+    }
+}
